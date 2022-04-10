cd92648baefcb06d24b616c97a03c32fb054b9c4
roroquentin <hasan.guzelmansur@icloud.com>
2022-04-10 12:37:11 +0300

    Angepasst, um Daten aus Firebase auf den neuesten Stand zu ziehen und aufzulisten.

--- MPark/ListParkViewController.swift
+++ MPark/ListParkViewController.swift
@@ -6,10 +6,15 @@
 //
 
 import UIKit
+import Firebase
 
 class ListParkViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var parkLotArray = [String]()
+    var statusArray = [String]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,20 +24,74 @@ class ListParkViewController: UIViewController, UITableViewDelegate, UITableView
         tableView.delegate = self
         tableView.dataSource = self
         
+        getDataFromFirestore()
+        
+    }
+    
+    func getDataFromFirestore() {
+        
+        let fireStoreDatabase = Firestore.firestore()
+        
+        fireStoreDatabase.collection("cams").addSnapshotListener { snapshot, error in
+            
+            if error != nil {
+                
+                self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error!")
+                
+            }else {
+                
+                if snapshot?.isEmpty != true && snapshot != nil {
+                
+                    for document in snapshot!.documents {
+                        
+                        let documentID = document.documentID
+                        
+                        if let parkLot = document.get("parklot") as? String {
+                            
+                            self.parkLotArray.append(parkLot)
+                            
+                        }
+                        
+                        if let status = document.get("status") as? String {
+                            
+                            self.statusArray.append(status)
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+                self.tableView.reloadData()
+                
+            }
+            
+        }
+        
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return statusArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ParkListCell
         
-        cell.parkPlatzLabel.text = "A7"
-        cell.statusLabel.text = "Frei"
+        cell.parkPlatzLabel.text = String(parkLotArray[indexPath.row])
+        cell.statusLabel.text = String(statusArray[indexPath.row])
         
         return cell
+        
+    }
+    
+    func makeAlert(titleInput:String, messageInput:String) {
+        
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
         
     }
 
