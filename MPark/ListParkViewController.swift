afa070b3a672673fcadf7cee3af62682e0c425f0
roroquentin <hasan.guzelmansur@icloud.com>
2022-04-10 11:19:57 +0300

    Tabelle und angepasste Liste hinzugefügt. ParkListCell VC wurde erstellt.

--- MPark/ListParkViewController.swift
+++ MPark/ListParkViewController.swift
@@ -7,23 +7,33 @@
 
 import UIKit
 
-class ListParkViewController: UIViewController {
+class ListParkViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ParkListCell
+        
+        cell.parkPlatzLabel.text = "A7"
+        cell.statusLabel.text = "Frei"
+        
+        return cell
+        
+    }
 
 }
---
+++ MPark/ParkListCell.swift
@@ -0,0 +1,32 @@
+//
+//  ParkListCell.swift
+//  MPark
+//
+//  Created by Hasan Güzelmansur on 10.04.2022.
+//
+
+import UIKit
+
+class ParkListCell: UITableViewCell {
+
+    @IBOutlet weak var parkPlatzLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    @IBAction func navigationBtn(_ sender: Any) {
+        
+        
+        
+    }
+    
+}
